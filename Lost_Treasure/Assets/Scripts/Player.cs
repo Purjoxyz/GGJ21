f35ad71ae9fe8d9d4067b05801670e86c4ab2ec8
Lauri Kosonen <lauri.kosonen@hotmail.com>
2021-01-29 22:58:52 +0200

    Added player movement and gold ball

---
+++ Lost_Treasure/Assets/Scripts/Player.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField, Tooltip("Player movement speed")]
+    private float moveSpeed = 1;
+
+    [SerializeField, Tooltip("Player rotation speed")]
+    private float rotationSpeed = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateMove();
+        UpdateRotation();
+    }
+
+    private void UpdateMove()
+    {
+        Vector3 moveDirection = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            moveDirection += transform.forward;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            moveDirection += -1 * transform.forward;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            moveDirection += -1 * transform.right;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            moveDirection += transform.right;
+        }
+
+        moveDirection.Normalize();
+        Move(moveDirection);
+    }
+
+    private void UpdateRotation()
+    {
+        Vector3 rotationDirection = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rotationDirection += -1 * Vector3.up;
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rotationDirection += Vector3.up;
+        }
+
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + rotationDirection * rotationSpeed * Time.deltaTime);
+    }
+
+    private void Move(Vector3 direction)
+    {
+        transform.position += direction * moveSpeed * Time.deltaTime;
+    }
+}
