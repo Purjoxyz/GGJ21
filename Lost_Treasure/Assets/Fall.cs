5480b00d11bc6efc814ae3017bec01babeb42f75
Purjoxyz <pirjopistekosonen@gmail.com>
2021-01-30 11:51:57 +0200

    Distance between player and floor

---
+++ Lost_Treasure/Assets/Fall.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fall : MonoBehaviour
+{
+    public float gravity = 1;
+
+    // Start is called before the first frame update
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Physics.Raycast(transform.position + Vector3.up, Vector3.down,
+            out RaycastHit hitInfo, 1.1f, LayerMask.GetMask("Floor"));
+        if (hitInfo.collider != null)
+        {
+            //Debug.Log("Osui!");
+            Debug.Log(hitInfo.distance);
+        }
+    }
+}
