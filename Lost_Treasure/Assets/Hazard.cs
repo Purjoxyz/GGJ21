353770ba8748e17cd78b338890745869b5402f61
Purjoxyz <pirjopistekosonen@gmail.com>
2021-01-30 20:30:32 +0200

    Hazardous cube and script Hazard added

---
+++ Lost_Treasure/Assets/Hazard.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hazard : MonoBehaviour
+{
+    public int damage=1;
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            Player player = other.transform.parent.GetComponent<Player>();
+
+            if (player != null)
+                player.TakeDamage(damage);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
