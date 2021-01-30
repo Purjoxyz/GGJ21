92295a42b2f269b9cf5ce8d945a0381fceabf573
Lauri Kosonen <lauri.kosonen@hotmail.com>
2021-01-30 17:18:03 +0200

    Created camera controller script

---
+++ Lost_Treasure/Assets/CameraController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Vector3 cameraPositionOffset = new Vector3(0, 5, -5);
+
+    private Player player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<Player>();
+    }
+
+    private void LateUpdate()
+    {
+        FollowPlayer();
+    }
+
+    private void FollowPlayer()
+    {
+        Vector3 worldOffset = player.transform.right * cameraPositionOffset.x +
+                              player.transform.up * cameraPositionOffset.y +
+                              player.transform.forward * cameraPositionOffset.z;
+        transform.position = player.transform.position + worldOffset;
+
+        transform.rotation = Quaternion.Euler(
+            transform.rotation.eulerAngles.x,
+            player.transform.rotation.eulerAngles.y,
+            transform.rotation.eulerAngles.z);
+    }
+}
